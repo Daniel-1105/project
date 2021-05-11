57c7af7c9399b550beda3b6eae06cf08ae59dc77
Daniel-1105 <82467131+Daniel-1105@users.noreply.github.com>
2021-05-11 13:58:07 -0400

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- images.g.ts
+++ images.g.ts
@@ -75,22 +75,22 @@ namespace myImages {
 `;
             case "AY!95%":
             case "Asteroid 1":return img`
-. . . . . . . . . c c 8 . . . . 
-. . . . . . 8 c c c f 8 c c . . 
-. . . c c 8 8 f c a f f f c c . 
-. . c c c f f f c a a f f c c c 
-8 c c c f f f f c c a a c 8 c c 
-c c c b f f f 8 a c c a a a c c 
-c a a b b 8 a b c c c c c c c c 
-a f c a a b b a c c c c c f f c 
-a 8 f c a a c c a c a c f f f c 
-c a 8 a a c c c c a a f f f 8 a 
-. a c a a c f f a a b 8 f f c a 
-. . c c b a f f f a b b c c 6 c 
-. . . c b b a f f 6 6 a b 6 c . 
-. . . c c b b b 6 6 a c c c c . 
-. . . . c c a b b c c c . . . . 
-. . . . . c c c c c c . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . c c c c . . 
+. c c c c c . c c c c c f c c . 
+c c a c c c c c 8 f f c f f c c 
+c a f a a c c a f f c a a f f c 
+c a 8 f a a c a c c c a a a a c 
+c b c f a a a a a c c c c c c c 
+c b b a a c f 8 a c c c 8 c c c 
+. c b b a b c f a a a 8 8 c c . 
+. . . . a a b b b a a 8 a c . . 
+. . . . c b c a a c c b . . . . 
+. . . . b b c c a b b a . . . . 
+. . . . b b a b a 6 a . . . . . 
+. . . . c b b b 6 6 c . . . . . 
+. . . . . c a 6 6 b c . . . . . 
+. . . . . . . c c c . . . . . . 
 `;
         }
         return null;
